1d41133f3a00c7dae73db68ef0e8947b2886964e
Martin Burga <martin.burga@udla.edu.ec>
2023-11-27 16:30:09 -0500

    IV). Se añade comentarios y se arregla errores minimos del codigo.

--- Matriztrans.c
+++ Matriztrans.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 
 int main(void)
 {
@@ -14,26 +14,44 @@ int main(void)
     scanf("%d", &dy);
     int matrix[dx][dy];
 
+    //Se le pide al usuario las dimensiones de la matriz.
+
     for (int i = 0; i < dx; i++)
     {
         for (int j = 0; j < dy; j++)
         {
             numero = rand() % 100;
-            numero = rand() % (numero);    
+            numero = rand() % (numero+1);
             matrix[i][j] = numero;
         }
     }
 
-    printf("La matriz original es\n");
+    //Se generan numeros aleatorios del 0 al 100 y se va rellenando la matriz 
+
+    printf(" \n");
+    printf("La matriz original es:\n");
     for (int i = 0; i < dx; i++)
     {
         for (int j = 0; j < dy; j++)
         {
-            
+
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
 
+    // Se imprimen los valores de la matriz original. 
+    printf(" \n");
+    printf("La matriz transpuesta es:\n");
+    for (int j = 0; j < dy; j++)
+    {
+        for (int i = 0; i < dx; i++)
+        {
+
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+    //Se imprimen los mismos valores aleatorios generados pero se invierte la forma de escribir de la matriz. 
     return 0;
 }
